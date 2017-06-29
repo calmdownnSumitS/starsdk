66e6545d14ee9e2e2569e90c7d82f0f7d62aaf22
macserver <macserver@macservers-mac-pro.local>
2017-06-29 11:47:54 +0530

    Initial Commit

---
+++ startratingwithhcs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  startratingwithhcs
+//
+//  Created by macserver on 6/29/17.
+//  Copyright © 2017 macserver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ startratingwithhcs/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  startratingwithhcs
+//
+//  Created by macserver on 6/29/17.
+//  Copyright © 2017 macserver. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
